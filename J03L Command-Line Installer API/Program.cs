40b1239a258ef2b5b8d4c2c424f846dabdfda6b9
joela <joel.astley@gmail.com>
2019-12-01 14:22:11 +0000

    test commit

--- J03L Command-Line Installer API/Program.cs
+++ J03L Command-Line Installer API/Program.cs
@@ -8,13 +8,17 @@ namespace J03L_Command_Line_Installer_API
 {
     class Program
     {
-        // VARIABLES
+        // ASSIGN MAIN VARIABLES...
         static string VERSION = "0.0.1-initial";
         static void Main(string[] args)
         {
             Console.WriteLine("====[J03L Command-Line Installer API]====");
             Console.WriteLine("Version " + VERSION);
             Console.WriteLine("Author: Joel Astley");
+            Console.WriteLine("Author's GitHub:");
+            Console.WriteLine("https://github.com/joelastley555");
+            Console.WriteLine("Project's GitHub:");
+            Console.WriteLine("insert link here");
 
         }
     }
