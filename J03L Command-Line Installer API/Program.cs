6dd299cbe2d099a7a41f0b6edcf214cb2387b7c0
joela <joela@DESKTOP-U2NH5FE>
2019-12-01 13:28:21 +0000

    Add project files.

---
+++ J03L Command-Line Installer API/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J03L_Command_Line_Installer_API
+{
+    class Program
+    {
+        // VARIABLES
+        static string VERSION = "0.0.1-initial";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====[J03L Command-Line Installer API]====");
+            Console.WriteLine("Version " + VERSION);
+            Console.WriteLine("Author: Joel Astley");
+
+        }
+    }
+}
