5c4299f767db59bb18082c43adb559820fadf8ca
joela <joel.astley@gmail.com>
2019-12-01 15:01:01 +0000

    Inserted link to license

--- J03L Command-Line Installer API/Program.cs
+++ J03L Command-Line Installer API/Program.cs
@@ -8,18 +8,21 @@ namespace J03L_Command_Line_Installer_API
 {
     class Program
     {
-        // ASSIGN MAIN VARIABLES...
+        // ASSIGN MAIN VARIABLES.
         static string VERSION = "0.0.1-initial";
         static void Main(string[] args)
         {
-            Console.WriteLine("====[J03L Command-Line Installer API]====");
-            Console.WriteLine("Version " + VERSION);
-            Console.WriteLine("Author: Joel Astley");
-            Console.WriteLine("Author's GitHub:");
-            Console.WriteLine("https://github.com/joelastley555");
-            Console.WriteLine("Project's GitHub:");
-            Console.WriteLine("insert link here");
-
+            Console.WriteLine(@"====[J03L Command-Line Installer API]====
+Version " + VERSION + @"
+Author: Joel Astley
+Author's GitHub:
+https://github.com/joelastley555
+Project's GitHub:
+https://github.com/joelastley555/J03L-CL-Installer-API
+License:
+https://github.com/joelastley555/J03L-CL-Installer-API/blob/master/LICENSE");
+            
+            
         }
     }
 }
